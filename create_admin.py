a01b8f586ff33a744f48f9d682e511361a5bc0a5
Baoishere <143436102+Baoishere@users.noreply.github.com>
2025-07-28 18:30:46 +0700

    Update create_admin.py

--- create_admin.py
+++ create_admin.py
@@ -3,21 +3,22 @@ import mysql.connector
 from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
-app.secret_key = '2200001314'
+app.secret_key = '' # Đặt secret key cho Flask
+password = '' # Đặt mật khẩu cho admin
 
 # Kết nối trực tiếp với mysql.connector thay vì Flask-MySQLdb
 db = mysql.connector.connect(
     host="localhost",
-    user="root",
-    password="baoishere",  # hoặc password="" nếu XAMPP không có password
-    database="parkingdb"
+    user="root", 
+    password="",  # hoặc password="" nếu XAMPP không có password
+    database="" # Đặt tên database của bạn ở đây, ví dụ: "parkingdb"
 )
 
 try:
     cursor = db.cursor()
     cursor.execute(
         "INSERT INTO users (username, password) VALUES (%s, %s)",
-        ('admin', generate_password_hash('123456'))
+        ('admin', generate_password_hash(password, method='sha256'))
     )
     db.commit()
     print("Admin user created successfully!")
